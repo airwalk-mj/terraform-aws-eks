1ec7f8a55ec150498cf581952cd2b262b0d67fbb
Mark James <mark.james@airwalkconsulting.com>
2020-05-13 12:18:25 +0100

    .

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -40,8 +40,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  #cluster_name    = "test-eks-${random_string.suffix.result}"
-  #cluster_name    = "eks-demo"
+  cluster_name    = "eks-demo-${random_string.suffix.result}"
   cluster_version = "1.16"
 }
 
