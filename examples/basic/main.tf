0117a8514abd0265b8a6d879a56b7d9b8c9acc17
Mark James <mark.james@airwalkconsulting.com>
2020-05-13 17:43:54 +0100

    .

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -20,8 +20,7 @@ provider "null" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  #name = module.eks.cluster_id
-  name = king-vish
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
