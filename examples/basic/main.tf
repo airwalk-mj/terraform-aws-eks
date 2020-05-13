c2669e69672d9c6eb652f8a84f3cd64b2a2aa6a5
Mark James <mark.james@airwalkconsulting.com>
2020-05-13 11:29:05 +0100

    .

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
-#provider "aws" {
-#  version = ">= 2.28.1"
-#  region  = var.region
-#}
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = var.region
+}
 
 provider "random" {
   version = "~> 2.1"
@@ -16,10 +16,6 @@ provider "local" {
 }
 
 provider "null" {
-  version = "~> 2.1"
-}
-
-provider "template" {
   version = "~> 2.1"
 }
 
@@ -43,8 +39,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  #cluster_name = "test-eks-${random_string.suffix.result}"
-  cluster_name = "eks-demo"
+  cluster_name = "test-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -103,7 +98,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "demo-vpc"
+  name                 = "test-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
@@ -127,8 +122,7 @@ module "eks" {
   source       = "../.."
   cluster_name = local.cluster_name
   subnets      = module.vpc.private_subnets
-  cluster_version = "1.16"
-  
+
   tags = {
     Environment = "test"
     GithubRepo  = "terraform-aws-eks"
