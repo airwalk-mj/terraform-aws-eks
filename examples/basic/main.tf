975d76d4554d7d4f501b14afa76479c9aa2a218c
Mark James <mark.james@airwalkconsulting.com>
2020-05-13 12:24:51 +0100

    .

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -40,6 +40,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
+  vpc_name    = "airwalk-lab-${random_string.suffix.result}"
   cluster_name    = "airwalk-lab-${random_string.suffix.result}"
   cluster_version = "1.16"
 }
@@ -100,7 +101,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "test-vpc"
+  name                 = local.vpc_name
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
