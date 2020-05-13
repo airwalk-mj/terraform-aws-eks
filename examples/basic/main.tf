4cafc0408cdbb0a6f76a632b43320ab390e2a214
Mark James <mark.james@airwalkconsulting.com>
2020-05-13 17:51:58 +0100

    .

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -20,8 +20,7 @@ provider "null" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = "lab-cluster"
-  #name = module.eks.cluster_id
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
