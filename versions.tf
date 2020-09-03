e20aaca193fd16bb62886a338c0eb11bf56b39fa
Mark James <mark.james@airwalkconsulting.com>
2020-09-03 18:18:05 +0100

    .

--- versions.tf
+++ versions.tf
@@ -2,10 +2,11 @@ terraform {
   required_version = ">= 0.12.9"
 
   required_providers {
-    aws        = ">= 2.52.0"
+    aws        = ">= 3.4.0"
     local      = ">= 1.4"
+    template   = ">= 2.1"
     null       = ">= 2.1"
-    random     = ">= 2.1"
-    kubernetes = ">= 1.11.1"
+    random     = ">= 2.3"
+    kubernetes = ">= 1.13.1"
   }
 }
